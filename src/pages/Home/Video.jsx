38727b6aa7d629397b2fc60e796ebb0a40c839e5
APbigstar <anthony.pen1115@gmail.com>
2023-11-02 14:31:16 +0800

    updated video

--- src/pages/Home/Video.jsx
+++ src/pages/Home/Video.jsx
@@ -1,48 +1,56 @@
-import React, { } from "react";
-import ScrollyVideo from 'scrolly-video/dist/ScrollyVideo.cjs.jsx';
+import React from 'react';
+import { VideoScroll } from 'react-video-scroll';
+import PropTypes from 'prop-types';
+import HomeVideo from '../../assets/video/home_video_browser.mp4';
+import { makeStyles } from '@mui/styles';
+// eslint-disable-next-line react/prop-types
 
-import HomeVideoBrowser from "../../assets/video/home_video_browser.mp4";
-import FinancialVideo from "../../assets/video/Financial Services.mp4";
-import ProjectVideo from "../../assets/video/Project Management.mp4";
-
-const Video = () => {
-  const videos = [
-    HomeVideoBrowser,
-    FinancialVideo,
-    ProjectVideo
-  ];
-
-  const [currentVideoIndex, ] = React.useState(0);
-  // useEffect(() => {
-
-    // window.onwheel = e => {
-    //   if (e.deltaY >= 0) {
-    //     if(window.pageYOffset == 1926) {
-    //       if( currentVideoIndex < videos.length - 1 ) {
-    //         setCurrentVideoIndex( currentVideoIndex + 1);
-    //         window.scrollTo(0,0);
-    //       }
-    //     }
-    //     // scrollUp();
-    //   } else {
-    //     if(window.pageYOffset == 0) {
-    //       if( currentVideoIndex > 0 ) {
-    //         setCurrentVideoIndex( currentVideoIndex - 1);
-    //         window.scrollTo(0, window.pageYOffset);
-    //       }
-    //     }
-    //     // scrollDown();
-    //   }
-    // }
-
-    // Make sure the DOM is loaded before interacting with it
-  // }, [currentVideoIndex, videos]);
+const useStyle = makeStyles({
+  hideContent: {
+    height: "0px!important"
+  }
+})
+// eslint-disable-next-line react/prop-types
+function Video(props) {
+  const {isMenuOpen} = props
+  const classes = useStyle();
+  const setHeight = React.useRef();
+  
+  // eslint-disable-next-line no-unused-vars
+  const setStyles = (wrapperEl, videoEl, playbackRate) => {
+    console.log(wrapperEl);
+    setHeight.current.style.height = (playbackRate * videoEl.duration + 100) +"px"
+  }
+  
 
   return (
-    <div className="scrolly-container" style={{ height: '300vh' }}>
-      <ScrollyVideo  src={videos[currentVideoIndex]} />
-    </div>
+    <>
+      <div style={{ position: "fixed", top: "0",}}>
+        <VideoScroll
+          // eslint-disable-next-line react/prop-types
+          onLoad={(props) => setStyles(props.wrapperEl, props.videoEl, props.playbackRate)}
+          playbackRate={1000}
+          id="one"
+        >
+          <video
+            id="v0"
+            tabIndex="0"
+            preload="preload"
+            style={{ width: '100vw', height: "100vh", objectFit: 'fill' }}
+            playsInline
+          >
+            <source type="video/mp4" src={HomeVideo} />
+          </video>
+        </VideoScroll>
+
+      </div>
+      <div ref={setHeight} className={`${isMenuOpen ? classes.hideContent : ""}`}></div>
+
+    </>
+
   );
 }
-
 export default Video;
+Video.propTypes = {
+  isMenuOpen:PropTypes.bool
+}
